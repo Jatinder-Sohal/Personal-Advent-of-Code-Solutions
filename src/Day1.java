6f7064442fc50da115ecc8c2ccd2504786fb14aa
ZKAC245 <jatinder.sohal.2021@live.rhul.ac.uk>
2024-06-17 14:57:24 +0100

    Changed Day1 solution to include part 2: Allowing numbers written out to be accepted as digits using 2 hashmaps.

--- src/Day1.java
+++ src/Day1.java
@@ -1,23 +1,53 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day1 {
+    static String digitOne = "";
+    static String digitTwo = "";
+
     public static void main(String[] args) throws IOException {
+        Map<String, String> mapStringNum = new HashMap<>();
+        mapStringNum.put("on", "one");
+        mapStringNum.put("tw", "two");
+        mapStringNum.put("th", "three");
+        mapStringNum.put("fo", "four");
+        mapStringNum.put("fi", "five");
+        mapStringNum.put("si", "six");
+        mapStringNum.put("se", "seven");
+        mapStringNum.put("ei", "eight");
+        mapStringNum.put("ni", "nine");
+
+        Map<String, String> stringNumToInt = new HashMap<>();
+        stringNumToInt.put("one","1");
+        stringNumToInt.put("two","2");
+        stringNumToInt.put("three","3");
+        stringNumToInt.put("four","4");
+        stringNumToInt.put("five","5");
+        stringNumToInt.put("six","6");
+        stringNumToInt.put("seven","7");
+        stringNumToInt.put("eight","8");
+        stringNumToInt.put("nine","9");
+
         File file = new File("input1.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
+        String line;
         char letter;
-        String digitOne = "";
-        String digitTwo = "";
         int total = 0;
-        while ((st = br.readLine()) != null){
-            for(int i = 0; i < st.length(); i++){
-                letter = st.charAt(i);
+        while ((line = br.readLine()) != null){
+            for(int i = 0; i < line.length(); i++){
+                letter = line.charAt(i);
                 if (Character.isDigit(letter)){
-                    if (digitOne == "")
-                        digitOne = letter+"";
-                    else
-                        digitTwo = letter+"";
+                    updateVariables(letter+"");
+                }else if (line.substring(i).length() > 2){
+                    String possibleNumber = line.substring(i, i+2);
+                    String stringNum = mapStringNum.get(possibleNumber);
+                    if (stringNum != null && line.substring(i).length() >= stringNum.length()){
+                        possibleNumber = line.substring(i, i + stringNum.length());
+                        if (possibleNumber.equals(stringNum)){
+                            updateVariables(stringNumToInt.get(stringNum));
+                        }
+                    }
                 }
             }
             if (digitTwo.equals(""))
@@ -27,5 +57,12 @@ public class Day1 {
             digitTwo = "";
         }
         System.out.println(total);
+        br.close();
+    }
+    public static void updateVariables(String letter){
+        if (digitOne.equals(""))
+            digitOne = letter;
+        else
+            digitTwo = letter;
     }
 }
